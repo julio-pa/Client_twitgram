11dc18720205dc3202b814993c6e2aa41d421e51
Julio Patiño <franjp132@gmail.com>
2023-09-20 17:21:37 -0500

    Connect whit the python server and consume the api

--- src/components/home/Posts.jsx
+++ src/components/home/Posts.jsx
@@ -1,13 +1,34 @@
 import PostCard from '../posts/PostCard';
+import { get_tweets_list } from '../../redux/actions/tweets/tweets'
+import { useEffect } from 'react';
+import { connect } from 'react-redux'
 
-const Posts = () => {
+
+const Posts = ({
+  get_tweets_list,
+  posts,
+}) => {
+
+  useEffect(() => {
+    get_tweets_list()
+  }, []);
+
   return (
     <div>
-      <PostCard />
-      <PostCard />
-      <PostCard />
+      {posts && posts.map((post, index) => (
+        <PostCard data={post} key={index} index={index} />
+      ))}
     </div>
   )
 }
 
-export default Posts;
+const mapStateToProps = state => ({
+  posts: state.tweets.tweets_list,
+  count: state.tweets.count,
+  next: state.tweets.next,
+  previous: state.tweets.previous,
+})
+
+export default connect(mapStateToProps, {
+  get_tweets_list,
+})(Posts);
--- src/components/posts/PostCard.jsx
+++ src/components/posts/PostCard.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import Avatar from "../home/Avatar";
 
 
-const PostCard = () => {
+const PostCard = ({ data, index }) => {
 
   const [like, setLike] = useState(false);
   const [styleLike, setStyleLike] = useState('');
@@ -37,12 +38,14 @@ const PostCard = () => {
           more_horiz
         </span>
       </div>
-      <div className="my-3 flex flex-col justify-center items-center md:px-14">
+      <div className="my-3 flex flex-col justify-start md:px-14">
         <div className="my-3" >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          {data.description}
         </div>
         <div className="w-full">
-          <img src="https://th.bing.com/th/id/OIP.4F0j9TOEdVKB_bfu8wU_-QHaFj?pid=ImgDet&rs=1" className="h-80 w-full object-fill rounded-2xl cursor-pointer" />
+          {data.thumbnail
+            ? <img id={index} src={data?.thumbnail} className="h-80 w-full object-fill rounded-2xl cursor-pointer" />
+            : null}
         </div>
       </div>
       <div className="flex justify-between md:px-14 my-6 border-t border-gray-400 pt-6">
@@ -56,7 +59,6 @@ const PostCard = () => {
 
 }
 
-import React from 'react';
 
 const PostButtom = ({ icon, count, style, turn }) => {
   return (
