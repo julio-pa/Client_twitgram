c21d4636e8c0d4710a7b4a73d8c9a6fa69df886f
Julio Patiño <franjp132@gmail.com>
2023-09-22 18:17:50 -0500

    Make the login page

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,8 +2,20 @@
 
 const LoginForm = () => {
   return (
-    <div className='border h-3/4 w-2/4'>
-      LoginForm
+    <div className=' w-3/4 p-3 flex flex-col justify-center gap-3'>
+      <form className="flex flex-col gap-3 ">
+        <label className="font-semibold text-lg">Username</label>
+        <input type="text" maxLength="64" className="text-black rounded-full border-4 border-transparent p-1 outline-none focus:border-cyan-400" placeholder="Username" required />
+        <label className="font-semibold text-lg">Email</label>
+        <input type="email" className="text-black rounded-full border-4 border-transparent p-1 outline-none focus:border-cyan-400" placeholder="Email" required />
+        <label className="font-semibold text-lg">Password</label>
+
+        <input type="password" className="text-black rounded-full border-4 border-transparent p-1 outline-none focus:border-cyan-400" placeholder="Password" required />
+        <button type="submit" className="bg-cyan-400 rounded-full p-3 font-semibold text-xl hover:bg-blue-400">Create Account</button>
+      </form>
+      <p className=" text-gray-400 text-xs">By registering, you agree to the Terms of Service and Privacy Policy, including the Use of Cookies policy.</p>
+      <div className="text-2xl font-semibold">Already have an account?</div>
+      <button className="bg-transparent border rounded-full p-3 font-semibold text-xl hover:bg-blue-400">Sign in</button>
     </div>
   );
 }
--- src/containers/pages/Login.jsx
+++ src/containers/pages/Login.jsx
@@ -1,17 +1,18 @@
 import LoginForm from "../../components/login/LoginForm";
-import Layout from "../../hocs/layouts/Layout";
+// import Layout from "../../hocs/layouts/Layout";
 import logo from '../../assets/twitgram_logo.svg'
 
 const Login = () => {
   return (
-    <Layout>
-      <img className="w-2/4 text-red-500" src={logo} />
-      <div className="w-full flex flex-col gap-3 justify-center">
+    <div className="flex pr-6">
+      <img className="w-3/4 text-red-500 max-md:hidden" src={logo} />
+      <div className="w-full flex flex-col gap-3 justify-center max-md:pl-8">
         <h3 className="text-7xl font-bold ">{"What's happenning"} <br />now</h3>
         <h3 className="text-4xl font-bold py-6">Join Today</h3>
         <LoginForm />
+
       </div>
-    </Layout>
+    </div>
   );
 }
 
