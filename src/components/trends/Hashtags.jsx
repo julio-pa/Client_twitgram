207d1d56ab2efbebea21b9318c066b24a666a25a
Julio Patiño <franjp132@gmail.com>
2023-10-05 15:38:58 -0500

    New Hashtags

--- src/components/trends/Hashtags.jsx
+++ src/components/trends/Hashtags.jsx
@@ -13,10 +13,22 @@ const Hashtags = () => {
         <h3 className="text-cyan-400 font-semibold text-xl">Views</h3>
       </div>
       <div className="flex justify-between my-3">
-        <div className="flex gap-1 ">
+        <div className="flex flex-col gap-1 ">
           <h3>#IamTheBest</h3>
         </div>
         <h3>1.1M</h3>
+      </div>
+      <div className="flex justify-between my-3">
+        <div className="flex flex-col gap-1 ">
+          <h3>{"#GunsN'Roses"}</h3>
+        </div>
+        <h3>2.2M</h3>
+      </div>
+      <div className="flex justify-between my-3">
+        <div className="flex flex-col gap-1 ">
+          <h3>#Lentejas</h3>
+        </div>
+        <h3>3.3M</h3>
       </div>
     </div>
   );
